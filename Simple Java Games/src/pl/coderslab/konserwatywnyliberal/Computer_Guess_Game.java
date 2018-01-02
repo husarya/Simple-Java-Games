aad4088991b5b08199fe7b1ce31be489b9a66235
Lukasz Skworc <lukaszskworc@gmail.com>
2018-01-02 01:58:14 +0100

    Bug fix

--- Simple Java Games/src/pl/coderslab/konserwatywnyliberal/Computer_Guess_Game.java
+++ Simple Java Games/src/pl/coderslab/konserwatywnyliberal/Computer_Guess_Game.java
@@ -9,49 +9,60 @@ public class Computer_Guess_Game {
 
 	public static void main(String[] args) {
 
+		scan = new Scanner(System.in);
+		
+		Integer number = 500;
+		Integer min = 1;
+		Integer max = 1000;
+		Integer counter = 1;
+		
+		String input = "";
+		String bigger = "more";
+		String smaller = "less";
+		String luckyStrike = "bingo";
+		
 		System.out.println("Think about a number from 1 - 1000 range. Computer will try to guess it in 10 attempts");
 		System.out.println("Cheating will make computer cry");
-		
-		Integer counter = 1;
-		Integer number = 500;
-		
-		do{
+		System.out.println("========================================================================================");
+
+		while (true) {
 			
-			scan = new Scanner(System.in);
-				
-			Integer min = 1;
-			Integer max = 1000;
-			
-			System.out.println("===========================================================================");
 			System.out.println("Attempt " + counter);
-			System.out.println("Computer says the number you was thinking about is " + number);
-			System.out.println("Is the number: smaller | bigger | or maybe I have guessed in first try :D?");
+			System.out.println("Computer says your imagined number is " + number + " ?");
+			System.out.println("Type: | more | less | bingo |");
+			System.out.println("=======================================================");
 			
-			String more = "bigger";
-			String less = "smaller";
-			String bingo = "bingo";
-			String command = "";
-			command = scan.next();
+			input = scan.next();
+			System.out.println("");
 			
-			if (command.equals(more)) {
+			if (input.equals(bigger)) {
 				counter++;
 				min = number;
 				number = (number + max) / 2;
-			}else if (command.equals(less)) {
+				
+			}else if (input.equals(smaller)) {
 				counter++;
 				max = number;
 				number = (min + number) / 2;
-			}else if (command.equals(bingo)) {
-				System.out.println("Computer guessed your number in attempt nr." + counter);
-				break;
-
+				
+			}else if (input.equals(luckyStrike)) {
+				System.out.println("Computer guessed in " + counter + " attempts");
+				System.out.println("");
+				System.exit(0);
+				scan.close();
+					
 			}else {
-				System.out.println("=== System ERROR ===");
 				System.out.println("Invalid command");
-				System.out.println("Valid commands are: bigger | smaller | bingo ");
+				System.out.println("");
+			
 			}	
-		  }while (counter<11); 
-			    System.out.println("Well it is not polite to cheat, you made computer cry :( ");
+		while (counter > 10) {
+			System.out.println("You cheated ! You made computer cry :'(");
+			System.out.println("");
+			scan.close();
+			System.exit(0);
+		}
+		}
 		
 	}
 }
